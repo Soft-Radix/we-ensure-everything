f40273ba6cfc593206e2ce7369670bd9571b3322
uday <uday@softradix.in>
2026-03-25 10:57:58 +0530

    feat: Configure database port and SSL options for the Sequelize connection.

--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -7,8 +7,19 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "3306"),
     dialect: "mysql",
     dialectModule: mysql2,
+    dialectOptions: {
+      ssl:
+        process.env.DB_SSL === "true" ||
+        (process.env.DB_HOST &&
+          !["localhost", "127.0.0.1"].includes(process.env.DB_HOST))
+          ? {
+              rejectUnauthorized: false,
+            }
+          : undefined,
+    },
     logging: process.env.NODE_ENV === "development" ? console.log : false,
     pool: {
       max: 10,
